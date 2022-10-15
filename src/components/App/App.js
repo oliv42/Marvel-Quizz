96042e013fc912beec1712fae741769a7b8d83f9
Olivier.r <olivier.reboul@gmail.com>
2022-10-15 19:08:30 +0200

    landing add to app and test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
     import '../../App.css';
+import Landing from '../Landing/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
+        <Landing />
       </div>
     );
   }
