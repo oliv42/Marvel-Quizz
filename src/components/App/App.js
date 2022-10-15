12de3185e00c698987a9b99a903f081f3619c64e
Olivier.r <olivier.reboul@gmail.com>
2022-10-15 18:58:37 +0200

    hearder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import Header from '../Header/Header';
+    import '../../App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Header></Header>
       </div>
     );
   }
