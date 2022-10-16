6b9567b31735854f3f327de59f2d2f982884e40d
Olivier.r <olivier.reboul@gmail.com>
2022-10-16 17:07:17 +0200

    create React fuct Welcome

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
-    import '../../App.css';
 import Landing from '../Landing/Landing';
 import Footer from '../Footer/Footer';
+import Welcome from '../Welcome/Welcome';
+import '../../App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Welcome />
         <Landing />
         <Footer />
       </div>
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+    return (
+        <div className="quiz-bg">
+            <div>Welcome</div>
+        </div>
+    );
+};
+
+export default Welcome;
