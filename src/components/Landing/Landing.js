0460118fa16878874fb7e50e16e3596f9f15b704
Olivier.r <olivier.reboul@gmail.com>
2022-10-16 16:25:50 +0200

    Add button and animation on wolferin

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from "react";
 
 const Landing = () => {
-  return (
-    <main className='welcomePage'>
-    
-    </main>
-  )
-}
+    const refWolverine = useRef(null);
+    const [btn, setBtn] = useState(false);
+    useEffect(() => {
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true);
+        }, 3000);
+    }, []);
 
-export default Landing
+    const displayBtn = btn && (
+        <React.Fragment>
+            <div className="leftBox">
+                <button className="btn-welcome">Inscrition</button>
+            </div>
+            <div className="rightBox">
+                <button className="btn-welcome">Connexion</button>
+            </div>
+        </React.Fragment>
+    );
+    return (
+        <main ref={refWolverine} className="welcomePage">
+            {displayBtn}
+        </main>);
+};
+
+export default Landing;
