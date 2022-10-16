3602a8cc7531db15e0204df580925306b977128b
Olivier.r <olivier.reboul@gmail.com>
2022-10-16 22:05:08 +0200

    errorPage style and content

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,9 +1,23 @@
 import React from "react";
+import batamn from "../../images/batman.png";
+
+const centerH2 = {
+    textAlign: "center",
+    marginTop: "50px   ",
+};
+
+const centerImg = {
+    display: "top",
+    margin: "40px auto",
+};
 
 const ErrorPage = () => {
     return (
         <div className="quiz-bg">
-            <div className="container">ErrorPage</div>{" "}
+            <div className="container">
+                <h2 style={centerH2}>Oups cette image n'existe pas</h2>
+                <img src={batamn} alt="Error page" style={centerImg} />
+            </div>{" "}
         </div>
     );
 };
